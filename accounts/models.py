b03a5293aaf5db38c5420a083c10d78a9bf63526
alijaberi <alijaberi.web@gmail.com>
2022-11-27 18:32:57 +0330

    add user class by 2 fields email and phone

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from phonenumber_field.modelfields import PhoneNumberField
 
-# Create your models here.
+
+
+
+
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+    phone = PhoneNumberField(region="IR",null=False, blank=False, unique=True)
